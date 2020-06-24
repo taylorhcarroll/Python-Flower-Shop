1d7d9423e525163c52bd8e39d0a17d873951cced
Taylor Harrison Carroll <taylorhcarroll1@gmail.com>
2020-06-24 16:28:51 -0500

    reworked interface classes on arrangement level, not flower level

--- arrangement.py
+++ arrangement.py
@@ -1,7 +1,35 @@
+from flowers import Flower
+
+
 class Arrangement:
 
     def __init__(self):
         self.__flowers = []
+# enhance means you are adding a flower to this arrangement,
+# this is not meant to imply you are enhancing an existing flower in this arrangement
 
     def enhance(self, flower):
         self.__flowers.append(flower)
+
+
+class ValentinesDayArrangement(Arrangement):
+    def __init__(self):
+        super().__init__()
+
+    def enhance(self, flower):
+        try:
+            if flower.stem_length == 4 and flower.container_refridgerated == True:
+                self.flowers.add(flower)
+        except AttributeError as ex:
+            print(f'{flower} cannot be added to {self}')
+
+class MothersDayArrangement(Arrangement):
+    def __init__(self):
+        super().__init__()
+
+    def enhance(self, flower):
+        try:
+            if flower.stem_length == 4 and flower.container_refridgerated == True:
+                self.flowers.add(flower)
+        except AttributeError as ex:
+            print(f'{flower} cannot be added to {self}')
--- flowers.py
+++ flowers.py
@@ -1,22 +1,19 @@
+
 class Flower:
     def __init__(self, length, container):
         self.stem_length = length
-        self.container = container
+        self.container_refridgerated = container
 
-
-class IValentineFlower:
-    def __init__(self):
+    def create_mothersDay(self):
         self.stem_length = 7
-        self.container_refrigerated = False
+        self.container_refridgerated = False
 
-
-class IMothersDayFlower:
-    def __init__(self):
+    def create_valentine(self):
         self.stem_length = 4
-        self.container_refrigerated = True
+        self.container_refridgerated = True
 
 
-class Rose(IValentineFlower):
+class Rose(Flower):
     def __init__(self, color):
         super().__init__()
         self.color = color
@@ -25,4 +22,10 @@ class Rose(IValentineFlower):
         return f"{self.color} Rose"
 
 
-class Lilly()
+class Lilly(Flower):
+    def __init__(self, color):
+        super().__init__()
+        self.color = color
+
+    def __str__(self):
+        return f"{self.color} Lilly"
